21c957074a1ecef5d55c65667c571a5ad61f2f3a
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-18 19:00:41 +0530

    Added functionality to check size of tree

--- src/main/java/BinaryTreeMain.java
+++ src/main/java/BinaryTreeMain.java
@@ -4,6 +4,17 @@ public class BinaryTreeMain {
         b.add(56);
         b.add(30);
         b.add(70);
+        b.add(22);
+        b.add(40);
+        b.add(60);
+        b.add(95);
+        b.add(11);
+        b.add(65);
+        b.add(3);
+        b.add(16);
+        b.add(63);
+        b.add(67);
         b.display();
+        System.out.println("THE SIZE OF THE TREE IS "+b.size());
     }
 }
--- src/main/java/MyBinaryTree.java
+++ src/main/java/MyBinaryTree.java
@@ -12,20 +12,34 @@ public class MyBinaryTree<T extends Comparable<T>> {
             root = new MyNode(key);
             return root;
         }
-        if (root.data.compareTo(key)>0)
+        if (root.data.compareTo(key) > 0)
             root.left = add(root.left, key);
         else
             root.right = add(root.right, key);
         return root;
     }
+    public int size() {
+        return(size(root));
+    }
+    public int size(MyNode node) {
+        if (node == null) return(0);
+        else {
+            return(size(node.left) + 1 + size(node.right));
+        }
+    }
     void display() {
         display(root);
     }
-        void display(MyNode root) {
+
+    void display(MyNode root) {
         if (root != null) {
-            System.out.println(root.data);
+            System.out.println(root.data+" --> ");
             display(root.left);
             display(root.right);
+        }
+        else
+        {
+            System.out.print("/");
         }
     }
 
